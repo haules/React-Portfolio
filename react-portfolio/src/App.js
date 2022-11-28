009d703c69a185bbe4c0bfc3ade2b1cc31ea8de0
Alex <alex.ivan-cic@ibm.com>
2022-11-28 09:56:32 +0000

    NavBar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,30 +1,43 @@
 import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
 
 function App() {
-const [selectedPage, setSelectedPage] = useState('home');
-const [isTopOfPage, setIsTopOfPage] = useState(true);
-const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
-useEffect(() => {
-  const handleScroll = () => {
-    if(window.scrollY === 0) setIsTopOfPage(true);
-    if(window.scrollY !== 0) setIsTopOfPage(false);
-  }
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
 
-  window.addEventListener("scroll", handleScroll);
-  return () => window.removeEventListener("scroll", handleScroll);
-},[]);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar 
-      isTopOfPage={isTopOfPage}
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
-      <div className="w5/6 mx-auto md:h-full">
+      <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
 
+        <Landing setSelectedPage={setSelectedPage} />
       </div>
     </div>
   );
