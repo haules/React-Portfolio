a1170b4ac8dbff19bd70161bac717fe3c0a23c1b
Alex <alex.ivan-cic@ibm.com>
2022-11-29 17:32:29 +0000

    resolved

--- realestate/utils/fetchApi.js
+++ realestate/utils/fetchApi.js
@@ -2,11 +2,6 @@ import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com";
 
-// headers: {
-//     'X-RapidAPI-Key': 'e04e572ae5msha4456b17f2d72d9p1d0819jsnf33a59138609',
-//     'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
-//   }
-
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
