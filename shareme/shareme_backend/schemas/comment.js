309a93eec1ab30fce42213855ccb26bd140a52c4
Alex <alex.ivan-cic@ibm.com>
2022-12-05 12:02:53 +0000

    backend finished

---
+++ shareme/shareme_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default{
+    name: 'comment',
+    ttle: 'Comment',
+    type:'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string'
+        }
+    ]
+}
